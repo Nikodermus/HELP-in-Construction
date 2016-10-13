82b7d65ea41fb68a1a545e964d2b1beb05a33ea3
Nicolas M. Pardo <nicolasm@nikodermus.media>
2016-10-13 01:33:14 -0400

    v1.0

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php 
+//This meta info should change for each page
+$title = 'Fundación HELP | Página en construcción' ; 
+$description = 'FUNDACION HELP es una organización de Educación e Investigación, habilitación y rehabilitación, sin ánimo de lucro, de utilidad común, de carácter privado, que promueve el mejoramiento de las condiciones, intelectuales, técnicas, tecnológicas, científicas, laborales, culturales, económicas, éticas y sociales de niños, jóvenes y adultos ubicados en zonas urbanas y rurales de Colombia.' ; 
+$keywords = 'INSTITUCIÓN DE EDUCACIÓN, CAPACITACION, HABILITACION, REHABILITACION Y TRABAJO SOCIAL.' ; 
+      ?>
+    <?php include 'lib/head.php'; ?>
+
+        <section style="filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#e2525b', endColorstr='#c52a33',GradientType=1 );">
+            <div class="top-bar">
+                <div class="left">
+                    <h1 class="trajan">H.E.L.P</h1>
+
+                </div>
+                <div class="right">
+                    <p>Estamos creando
+                        <br>algo nuevo</p>
+                </div>
+            </div>
+            <div class="main-image">
+                <img src="img/logo.svg" alt="">
+            </div>
+            <div class="bottom-bar">
+
+
+                <div class="right">
+                    <p>¡Quiero ayudar!</p>
+                    <div class="form--container">
+                        <form action="">
+                            <div class="mail">
+                                <input type="email" placeholder="tucorreo@mail.com">
+                            </div>
+                            <div class="send">
+                                <input type="submit" value="" placeholder="">
+                                <i class="icon-right"></i>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="left">
+                    <div class="left--container">
+                        <div class="icon"><a href="" target="_blank"><i class="icon-facebook"></i></a></div>
+                        <div class="icon"><a href="" target="_blank"><i class="icon-instagram"></i></a></div>
+                        <div class="icon"><a href="" target="_blank"><i class="icon-twitter"></i></a></div>
+                        <div class="icon"><a href="" target="_blank"><i class="icon-youtube"></i></a></div>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+
+        <?php include 'lib/scripts.php'; ?>
---
+++ lib/scripts.php
@@ -0,0 +1,14 @@
+    <!--Bootstrap 4 Alpha-->
+    <script type="text/javascript" src="js/tether.min.js"></script> 
+    <script type="text/javascript" src="js/bootstrap.min.js"></script> 
+    
+    <!--Sudo Slider-->
+    <script type="text/javascript" src="js/sudoslider.min.js"></script>
+    
+    <!--Wow.js-->
+    <script type="text/javascript" src="js/wow.min.js"></script>
+
+    <!--Custom Scripts-->
+    <script type="text/javascript" src="js/actions.js"></script>
+</body>
+</html>
